db5eff9b54efa75eb4e10a297a277c4d3cc3b658
malachi26 <lyle.hart.3@gmail.com>
2015-04-15 12:44:16 -0500

    initial commit with Unit Test Project

---
+++ Exercise_5_3a/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_5_3a
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public Dictionary<Int16, Int16> CollectData()
+        {
+            return new Dictionary<short, short>();
+        }
+
+        public short ParseInput(string input)
+        {
+            short output;
+            if (!(short.TryParse(input, out output)))
+            {
+                return -1;
+            }
+            return output;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Excercise_5_3a;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void parseInput_text_Negative1()
+        {
+            var input = "text";
+            var program = new Program();
+            var output = program.ParseInput(input);
+            Assert.AreEqual(-1, output);
+        }
+
+        [TestMethod]
+        public void parseInput_number_number()
+        {
+            var input = "22";
+            var program = new Program();
+            var output = program.ParseInput(input);
+            Assert.AreEqual(22, output);
+        }
+    }
+}
