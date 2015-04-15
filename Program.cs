35d6d5ca785c2abeb49d93c3e847c841d8961ff7
malachi26 <lyle.hart.3@gmail.com>
2015-04-15 12:14:36 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_5_3a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public Dictionary<Int16, Int16> CollectData()
+        {
+            return new Dictionary<short,short>();
+        }
+
+        public short ParseInput(string input)
+        {
+            return 2;
+        }
+    }
+}
